cb0ef0aee8c6e0447b71e959e261139d35ec558a
swp1017 <swp1017@naver.com>
2025-04-30 03:56:40 +0900

    feat: 라우팅 설정을 위한 Configuration 클래스 구현

--- src/main/java/com/trendist/gateway_service/GatewayConfiguration.java
+++ src/main/java/com/trendist/gateway_service/GatewayConfiguration.java
@@ -1,4 +1,41 @@
 package com.trendist.gateway_service;
 
-public class GatewayConfiguraion {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
+public class GatewayConfiguration {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Autowired
+    public GatewayConfiguration(JwtAuthorizationFilter jwtAuthorizationFilter) {
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // User 서비스 라우트
+                .route("user_service_route", r -> r.path("/users/**")
+                        .filters(f -> f
+                                // JWT 인증 필터
+                                .filter(jwtAuthorizationFilter)
+                                .removeRequestHeader(HttpHeaders.COOKIE)
+                        )
+                        .uri("lb://USER-SERVICE"))
+
+                // 인증 필요 없는 라우트
+                .route("user_public_route", r -> r.path("/users/public/**")
+                        .filters(f -> f
+                                        .removeRequestHeader(HttpHeaders.COOKIE)
+                        )
+                        .uri("lb://USER-SERVICE"))
+
+                .build();
+    }
 }
