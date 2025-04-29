084a4ff0ea4f2d1c469cbd3be8344229bb11a240
swp1017 <swp1017@naver.com>
2025-04-30 04:00:15 +0900

    feat: JWT 토큰 인증시 토큰 만료 여부 예외처리 구현

--- src/main/java/com/trendist/gateway_service/exception/AccessTokenExpiredException.java
+++ src/main/java/com/trendist/gateway_service/exception/AccessTokenExpiredException.java
@@ -1,4 +1,8 @@
+// AccessTokenExpiredException.java
 package com.trendist.gateway_service.exception;
 
-public class AccessTokenExpiredException {
+public class AccessTokenExpiredException extends RuntimeException {
+    public AccessTokenExpiredException() {
+        super("Access token has expired");
+    }
 }
