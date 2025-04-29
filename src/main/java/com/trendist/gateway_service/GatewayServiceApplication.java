5aa395bf6ef90287b946fd3fea6e4813d1b0d245
swp1017 <swp1017@naver.com>
2025-04-30 03:58:14 +0900

    feat: Spring API Gateway 구현

--- src/main/java/com/trendist/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/trendist/gateway_service/GatewayServiceApplication.java
@@ -2,12 +2,28 @@ package com.trendist.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder(){
+		final WebClient.Builder builder = WebClient.builder();
+		return builder;
+	}
+
+	// RestClient.Builder 빈 추가
+	@Bean
+	public RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
