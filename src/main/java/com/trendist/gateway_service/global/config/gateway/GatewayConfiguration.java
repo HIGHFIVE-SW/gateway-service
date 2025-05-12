355de78329d570a6166d5351a15c21354032c0e8
1winhyun <jimking1@naver.com>
2025-05-12 20:48:18 +0900

    refactor: issue-service 인증 필요하도록 수정

--- src/main/java/com/trendist/gateway_service/global/config/gateway/GatewayConfiguration.java
+++ src/main/java/com/trendist/gateway_service/global/config/gateway/GatewayConfiguration.java
@@ -39,6 +39,14 @@ public class GatewayConfiguration {
 				.uri("lb://POST-SERVICE")
 			)
 
+			.route("issue_service_route", r -> r.path("/issues/**")
+				.filters(f -> f
+					// JWT 인증 필터
+					.filter(jwtAuthorizationFilter)
+					.removeRequestHeader(HttpHeaders.COOKIE)
+				)
+				.uri("lb://ISSUE-SERVICE"))
+
 			// 인증 필요 없는 라우트
 			.route("user_public_route", r -> r.path("/users/public/**")
 				.filters(f -> f
@@ -51,11 +59,6 @@ public class GatewayConfiguration {
 				.filters(f -> f
 					.removeRequestHeader(HttpHeaders.COOKIE))
 				.uri("lb://ACTIVITY-SERVICE"))
-
-			.route("issue-service_route", r -> r.path("/issues/**")
-				.filters(f -> f
-					.removeRequestHeader(HttpHeaders.COOKIE))
-				.uri("lb://ISSUE-SERVICE"))
 
 			.build();
 	}
